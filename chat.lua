5ec548ade63ca3421d9200a85b70d494023798a3
Trond A Ekseth <troeks@gmail.com>
2009-07-14 17:30:38 +0200

    Some minor chat tweaks.

---
+++ chat.lua
@@ -0,0 +1,23 @@
+-- Because it needs to be perfect.
+ChatFrame2:SetJustifyH"RIGHT"
+ChatFrame4:SetJustifyH"CENTER"
+
+-- This is mainly because we hate the FCF.
+FCF_ValidateChatFramePosition = Kousei.dummy
+
+function Kousei:ADDON_LOADED(event, addon)
+	if(addon == 'Kousei') then
+		-- chat channel colors
+		ChangeChatColor("CHANNEL1", 1, .14117647058824, .52549019607843)
+		ChangeChatColor("CHANNEL2", 0, .45, 1)
+
+		ChangeChatColor("OFFICER", .3, .75, .75)
+
+		ChangeChatColor("LOOT", 0, 2/3, 0)
+
+		ChangeChatColor("RAID", .8, .85, .8)
+		ChangeChatColor("RAID_LEADER", .8, .85, .8)
+
+		self:UnregisterEvent(event)
+	end
+end
