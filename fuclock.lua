a8845a4505dc529c37d32a2e923ba979f1f0fdfc
Trond A Ekseth <troeks@gmail.com>
2010-11-18 23:04:01 +0100

    Fix steino's stuff.

--- fuclock.lua
+++ fuclock.lua
@@ -1,12 +1,7 @@
 -- FU for removing the option to hide the clock!
-
-local f = CreateFrame("Frame", nil, UIParent)
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(self, event, name)
-	if name == "Blizzard_TimeManager" then
+Kousei:RegisterEvent("ADDON_LOADED", function(self, event, addon)
+	if(addon == "Blizzard_TimeManager") then
+		TimeManagerClockButton.Show = TimeManagerClockButton.Hide()
 		TimeManagerClockButton:Hide()
-		TimeManagerClockButton:SetScript("OnShow", function(self)
-			TimeManagerClockButton:Hide()
-		end)
 	end
 end)
